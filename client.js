d51bd5dd9aa43435cd98bcbf845e4b536c9fdc8d
Mathias Buus <mathiasbuus@gmail.com>
2022-08-31 19:43:50 +0200

    explicitly want cubic

--- client.js
+++ client.js
@@ -3,39 +3,41 @@ import net from 'net'
 
 const [,,publicKey, ip] = process.argv
 
+const onlyTCP = process.argv.includes('--only-tcp')
+const onlyUDX = process.argv.includes('--only-udx')
+
 const node = new DHT()
 const block = Buffer.alloc(4 * 1024 * 1024)
 
-await tcp()
+if (!onlyUDX) await tcp()
+if (!onlyTCP) await udx()
 
-console.log()
-
-await udx()
-
-console.log()
 console.log('Done! exiting...')
 
 process.exit()
 
-function udx () {
+async function udx () {
   console.log('Testing udx throughput... (encrypted)')
   console.log()
-  return test(node.connect(Buffer.from(publicKey, 'hex')))
+  await test(node.connect(Buffer.from(publicKey, 'hex')))
+  console.log()
 }
 
-function tcp () {
+async function tcp () {
   console.log('Testing tcp throughput... (raw)')
   console.log()
-  return test(net.connect(54540, ip))
+  await test(net.connect(54540, ip))
+  console.log()
 }
 
 async function test (socket) {
-  for (let i = 0; i < 25; i++) socket.write(block)
+  for (let i = 0; i < 100; i++) socket.write(block)
   socket.end()
-
-  socket.on('error', () => socket.destroy())
   socket.pipe(process.stdout)
 
-  return new Promise(resolve => socket.on('close', resolve))
+  return new Promise((resolve, reject) => {
+    socket.on('error', reject)
+    socket.on('close', resolve)
+  })
 }
 
